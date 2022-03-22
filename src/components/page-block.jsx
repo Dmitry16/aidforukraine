3cf89f6829bff810a6c6fb1ed55d05c181145149
Dmytro Davydov <dmytro_davydov@goodyear.com>
2022-03-22 11:50:18 +0100

    page-block added

---
+++ src/components/page-block.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+export default function PageBlock({children, img}) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: '100%',
+          height: '50vh',
+          margin: 0,
+          borderRadius: 0,
+        },
+      }}
+    >
+        <Paper elevation={0} sx={{
+            backgroundImage: img ? `url(${img})` : 'none',
+            backgroundSize: 'cover',
+        }}>
+            {children}
+        </Paper>
+    </Box>
+  );
+}
