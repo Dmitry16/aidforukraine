eb352271d4ab68b5ecdba7a19482d690ca704f96
Dmytro Davydov <dmytro.davydov@cheetahdigital.com>
2022-03-28 17:12:00 +0200

    BoxForm mobile layout

--- src/components/page-blocks/block5.jsx
+++ src/components/page-blocks/block5.jsx
@@ -1,11 +1,20 @@
 import Box from "@mui/material/Box";
 import PageBlock from "../page-block";
 import InventoryForm from "../../inventoryForm";
+import Typography from "@mui/material/Typography";
 
 export default function Block5() {
     return (
         <PageBlock>
             <Box id="cargo">
+            <Box
+                sx={{
+                    padding: "20px 30px",
+                    fontSize: { xs: "16px", md: "28px" },
+                }}
+            >
+                <Typography variant="h2">Cargo details</Typography>
+            </Box>
                 <InventoryForm />
             </Box>
         </PageBlock>
